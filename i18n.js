7e060985e830b0b524f3b3fd6b791d4576f2f0eb
Dominik Crha <crha@havit.cz>
2020-04-28 08:40:21 +0200

    Build?????

--- i18n.js
+++ i18n.js
@@ -1,6 +1,8 @@
 const NextI18Next = require("next-i18next").default;
 
 module.exports = new NextI18Next({
-	defaultLanguage: process.env.DEFAULT_LANGUAGE || "cs",
-	otherLanguages: [process.env.OTHER_LANGUAGE] || ["en"],
+	// defaultLanguage: process.env.DEFAULT_LANGUAGE || "cs",
+	// otherLanguages: [process.env.OTHER_LANGUAGE] || ["en"],
+	defaultLanguage: "cs",
+	otherLanguages: ["en"],
 });
